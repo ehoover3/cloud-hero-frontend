3c23c98299f6331df2789bdb4d21c7e6ce59135e
Eric Hoover <ehoover3@yahoo.com>
2023-05-15 16:47:40 -0400

    added tutorial

--- src/pages/HandsOnTutorials.tsx
+++ src/pages/HandsOnTutorials.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function HandsOnTutorials() {
   return (
     <div>
-        <h1>Hands On Tutorials</h1>
-        <div>Set up an API with API Gateway, Lambda, and DynamoDB</div>
+      <h1>Hands On Tutorials</h1>
+      <div>Set up an API with API Gateway, Lambda, and DynamoDB</div>
+      <div>Host a NodeJS App on Elastic Beanstalk</div>
+      <div>https://www.youtube.com/watch?v=O8lDdVbkms4</div>
     </div>
-  )
+  );
 }
 
-export default HandsOnTutorials
+export default HandsOnTutorials;
