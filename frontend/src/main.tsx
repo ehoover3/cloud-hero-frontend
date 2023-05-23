06a00a5a0be965cba4a62dfa5559a83339fac05e
ehoover3 <ehoover3@yahoo.com>
2023-05-23 15:05:24 -0400

    refactored with vite

---
+++ frontend/src/main.tsx
@@ -0,0 +1,10 @@
+
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import "./assets/stylesheets/main.css";
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <App />
+)
