d4a2378eba3db50640922702d92cd2410e240ff4
Eric Hoover <ehoover3@yahoo.com>
2023-06-06 15:17:40 -0400

    updated config files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,27 @@
 function Home() {
   return (
-    <div className="container">
+    <div className='container'>
       <div>
-        <h1 className="text-center">Cloud Cert Hero</h1>
-     
-     <div className="text-center">
-        <div id="home" className="flex-center flex-column" style={{margin:'10px'}}>
-          <a className="btn" href="/practiceexam">
-            Practice Exam
-          </a>
-          <a className="btn" href="/keyterms">
-            Key Terms
-          </a>
-          <a className="btn" href="/handsontutorials">
-            Hands-On Tutorials
-          </a>
-          <a className="btn" href="/other">
-            Other
-          </a>
+        <h1 className='text-center'>Cloud Cert Hero</h1>
+
+        <div className='text-center'>
+          <div id='home' className='flex-center flex-column' style={{ margin: "10px" }}>
+            <a className='btn' href='/practiceexam'>
+              Practice Exam
+            </a>
+            <a className='btn' href='/keyterms'>
+              Key Terms
+            </a>
+            <a className='btn' href='/handsontutorials'>
+              Hands-On Tutorials
+            </a>
+            <a className='btn' href='/other'>
+              Other
+            </a>
+          </div>
+          <div style={{ margin: "10px", width: "200px", outline: "1px solid black" }}>Question of the Day</div>
         </div>
-        <div style={{margin:'10px', width: "200px",outline:"1px solid black"}}>Question of the Day</div>
-      </div></div>
+      </div>
     </div>
   );
 }
