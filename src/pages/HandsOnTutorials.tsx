55b7eed35f460d079dbee438a438567351beb851
Eric Hoover <ehoover3@yahoo.com>
2023-06-06 15:32:09 -0400

    removed import react

--- src/pages/HandsOnTutorials.tsx
+++ src/pages/HandsOnTutorials.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-
 function HandsOnTutorials() {
   return (
     <div>
-        <h1>Hands On Tutorials</h1>
-        <div>Set up an API with API Gateway, Lambda, and DynamoDB</div>
+      <h1>Hands On Tutorials</h1>
+      <div>Set up an API with API Gateway, Lambda, and DynamoDB</div>
 
-        <div>Run a Serverless "Hello, World!" with AWS Lambda</div>
-        <div>https://aws.amazon.com/getting-started/hands-on/run-serverless-code/?ref=gsrchandson&id=updated</div>
+      <div>Run a Serverless "Hello, World!" with AWS Lambda</div>
+      <div>https://aws.amazon.com/getting-started/hands-on/run-serverless-code/?ref=gsrchandson&id=updated</div>
 
-        <div>Deploy a Web App on AWS Elastic Beanstalk</div>
-        <div>https://aws.amazon.com/getting-started/guides/deploy-webapp-elb/</div>
+      <div>Deploy a Web App on AWS Elastic Beanstalk</div>
+      <div>https://aws.amazon.com/getting-started/guides/deploy-webapp-elb/</div>
     </div>
   );
 }
